0eb146acf78d38d63b57c9a06cc3dd2fdca9d858
louzhuzz <liuyuyangxxx@163.com>
2025-03-31 11:01:01 +0800

    refactor(utils): 将工具函数提取到独立模块并更新引用
    将`showMessage`、`setCookie`和`getCookie`函数从HTML文件中提取到`utils.js`模块，并在相关文件中更新引用。此更改提高了代码的可维护性和复用性。

---
+++ js/utils.js
@@ -0,0 +1,34 @@
+// 公共工具函数模块
+
+// 显示消息
+function showMessage(msg, type, elementId = 'message') {
+    var $msg = $(`#${elementId}`);
+    $msg.text(msg).removeClass().addClass(`message ${type}`).show();
+}
+
+// 缓存参数到Cookie，有效期一年
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// 获取Cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+export { showMessage, setCookie, getCookie };
